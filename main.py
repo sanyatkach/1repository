694464561593ee5e73f3379660657b50987894cc
Oleksandr <sanya767670@gmail.com>
2020-02-19 17:41:33 +0200

    sml

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def hello():
     name = request.args.get("name", "World")
     return f'Oleksandr Tkachenko TS-91'
-
+@app.route('/first')
+def first():
+    n = None
+    num = 12
+    str = '213'
+    return f'{escape(n)}:{escape(num)},{escape(str)}'
 if __name__ == '__main__':
     app.run('0.0.0.0')
