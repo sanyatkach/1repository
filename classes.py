b906bc97d667ea03d5f08a83386e80b20506395e
Oleksandr <sanya767670@gmail.com>
2020-03-03 20:26:00 +0200

    init

---
+++ classes.py
@@ -0,0 +1,50 @@
+import random
+import itertools
+
+
+class Card:
+
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __str__(self):
+        return self.suit+self.rank
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        ranks = (2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A')
+        suits = ('\u2660', '\u2663', '\u2665', '\u2666')
+        for suit, rank in itertools.product(suits, ranks):
+            self.cards.append(Card(suit, rank))
+
+    def __str__(self):
+        return self.cards
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def pop(self):
+        try:
+            return str(self.cards.pop())
+        except IndexError:
+            self.__init__()
+            return "error"
+
+    def index(self, value):
+        try:
+            return str(self.cards[int(value) - 1])
+        except ValueError:
+            return "error"
+        except IndexError:
+            return "error"
+
+    def get_random(self):
+        try:
+            return str(random.choice(self.cards))
+        except IndexError:
+            self.__init__()
+            return "error"
+
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, escape, request, render_template  # @iLevanta
+from classes import Deck
 
 app = Flask(__name__)
+deck = Deck()
 
 @app.route('/')
 def hello():
@@ -12,14 +14,33 @@ def first():
     num = 12
     str = '213'
     return f'{escape(n)}:{escape(num)},{escape(str)}'
+
 @app.route('/second', methods=['GET','POST'])
 def second():
     if request.method == 'GET':
         return render_template('index.html')
     if request.method == 'POST':
-        text = request.form.get('text')
-        first = "".join(reversed(text))
-        return render_template('index.html', first=first)
-        # you can add text on this page again without reloading it
+        text = request.form.get("text")
+        rev1 = text[::-1]  # slice
+        rev2 = "".join(list(reversed(text)))  # reversed
+        rev3 = "".join([x for x in reversed(text)])  # comprehension
+        return f'{rev1},{rev2},{rev3}'
+
+@app.route('/third', methods=['GET','POST'])
+def third():
+    if request.method == 'GET':
+        return render_template('./third.html', content='\u0020', deck=str(deck))
+    else:
+        if "shuffle" in request.form:
+            deck.shuffle()
+            value = "Deck was shuffled"
+        elif "pop" in request.form:
+            value = deck.pop()
+        elif "get_random" in request.form:
+            value = deck.get_random()
+        else:
+            num = request.form.get("index")
+            value = deck.index(num)
+        return render_template('./third.html', content=value)
 if __name__ == '__main__':
     app.run('0.0.0.0')
